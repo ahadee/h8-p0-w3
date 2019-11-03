8af54b3dfac58b2431aa1dcce31c98874e665c49
ahadee <bagus.ardhika@gmail.com>
2019-11-03 23:38:17 +0700

    upoad exercise 15 week 3

--- exercise-15.js
+++ exercise-15.js
@@ -1,28 +1,41 @@
 function groupAnimals(animals) {
-    var arr = [];
-    var hasilSort = [];
-
-
+    var animalsBaru = [];
 
     for (var i=0; i<animals.length; i++) {
-        arr.push(animals[i])
+        var hasil = [];
+        var perKarakter = animals[i][0];
+        var perIndex = animals[i];
+        //var animalsBaru = [];
+
+        for (var j=animals.length-1; j>i; j--) {
+            if (perKarakter == animals[j][0]) {
+                perIndex = perIndex + ', ' + animals[j]
+                animals.splice(j,1);
+            }
+        }
+
+        hasil.push(perIndex)
+
+        animalsBaru.push(hasil)
+        // console.log(animalsBaru);
+
+    }
+     
+    //animalsBaru.sort();
+    var temp= [];
+    for (k=0; k<animalsBaru.length; k++) {
+        if (animalsBaru[k] > animalsBaru[k + 1]) {
+            temp = animalsBaru[k]
+            animalsBaru[k] = animalsBaru[k + 1]
+            animalsBaru[k + 1] = temp
+        }
     }
 
-    arr.sort();
-
-    // for (var j=0; j<arr.length; j++) {
-    //     if (animals[i][0] == arr[j][0]) {
-    //         hasilSort = 
-    //     }
-    // }
-
-    return arr;
-
-
+    return animalsBaru;
   }
   
   // TEST CASES
   console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-  // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+//   [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
   console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-  // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
+//   [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
